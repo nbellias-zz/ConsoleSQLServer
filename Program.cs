06f861f6bda4230db3b6ac512a36d00e95a1854e
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2021-08-06 13:01:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleSQLServer.DAL;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConsoleSQLServer
+{
+    class Program
+    {
+        private static IConfiguration _iconfiguration;
+        static void Main(string[] args)
+        {
+            GetAppSettingsFile();
+            PrintCountries();
+        }
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                                 .SetBasePath(Directory.GetCurrentDirectory())
+                                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }
+        static void PrintCountries()
+        {
+            var countryDAL = new CountryDAL(_iconfiguration);
+            var listCountryModel = countryDAL.GetList();
+            listCountryModel.ForEach(item =>
+            {
+                Console.WriteLine(item.Country);
+            });
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+    }
+}
